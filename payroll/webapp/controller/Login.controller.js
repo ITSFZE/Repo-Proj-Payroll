9d839573f6cdbc1f3f6a421c46e9c8452c484d86
Saravanan-Inflexion <58725773+Saravanan-Inflexion@users.noreply.github.com>
2019-12-20 17:26:43 +0530

    Login & Job master - Commit
    Commit for Mockup screen

---
+++ payroll/webapp/controller/Login.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+	"use strict";
+	return Controller.extend("com.Payroll.controller.Login", {
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf com.Payroll.view.Login
+		 */
+		onInit: function () {},
+		onPressLogin: function () {
+			this.getOwnerComponent().getRouter().navTo("JobMaster");
+		}
+
+	});
+});
