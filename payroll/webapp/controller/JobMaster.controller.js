b8f8431c47bc39ca848f0894ac14b1bfb9630c33
Saravanan-Inflexion <58725773+Saravanan-Inflexion@users.noreply.github.com>
2019-12-20 18:22:31 +0530

    Changes on View & Controllers namespaces for Login, CreateJob, EditJobMaster, & JobMaster
    Just Changed NameSpaces only

--- payroll/webapp/controller/JobMaster.controller.js
+++ payroll/webapp/controller/JobMaster.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("com.Payroll.controller.JobMaster", {
+	return Controller.extend("com.app.payroll.controller.JobMaster", {
 		onInit: function () {
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
 		},
